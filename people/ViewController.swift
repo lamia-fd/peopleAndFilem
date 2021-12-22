45d4d51294003cf1d81320ad2adfe58da45a7d6c
lamia <lamia.fd123@gmail.com>
2021-12-22 15:47:24 +0300

    Initial Commit

---
+++ people/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  people
+//
+//  Created by لمياء فالح الدوسري on 18/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
